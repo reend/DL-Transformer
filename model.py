59a1c9d9f542611e3c88fffec6d350f23e0195b9
K.P <prilipko.k@yoju.casino>
2024-01-29 23:02:23 +0100

    TransformerBlock

--- model.py
+++ model.py
@@ -40,3 +40,26 @@ class SelfAttention(nn.Module):
         # out shape: (N, query_len, heads, head_dim) then flatten last two dimensions
 
         out = self.fc_out(out)
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion*embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion*embed_size, embed_size)
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+        x = self.dropout(self.norm1(attention + query))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+
+        return out
